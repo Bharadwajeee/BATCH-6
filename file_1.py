6acb4a649c8607d818ecb566eff1582832b65934
Bharadwajeee <161969190+Bharadwajeee@users.noreply.github.com>
2024-03-11 15:57:26 +0530

    Add files via upload

---
+++ file_1.py
@@ -0,0 +1,126 @@
+#theory part
+"""
+bargained charecterestics
+
+controls and data structures -----> abc language
+strings and its format---> from c
+regular expressions--->pearl
+exception handling----> module 3
+ magic methods from----> forton
+ """
+"""
+python is a object oriented language
+               dynamically typed language
+               interpreted language
+               compiled language
+
+
+
+flavors of python
+
+--> cpython--written in c,most common implementationm of python
+--> jython--java implementation of python
+--> iron python-- implementation of python p[rogramming language which is tightly integrated with.net.
+--> brython-- browser python,runs in browser
+--> rubypython--bridge between python and ruby interpreters
+--> micro python-- runs in micro controllers
+
+"""
+
+# print has 5 parameters--(object,sep ,end,file,flush)
+#object--> the value to be printed is object
+#sep-->seperator
+#eg
+#a=8
+#b=9
+#c=89
+#print(a,b,b sep='$')
+
+#end--> whichnis used to add the string aaat the end of the value,
+##it will  not keep the command to stay in same line
+#eg
+#print(str(a),b,end='%%')
+#print(str(c))
+
+#file-->
+
+
+""""
+# ----> variables
+ --->variable is a name which is used to identify the value of the variable
+---> ?which is stored in in specific memeory location
+# rules to define variables
+#1.--> variable have to start with only alphabets,underscores
+#2.--> the char following the 1st char can be alphabets,numbers,underscores
+#3.--> no spaces are allowed
+#4.--> no special charecters  like @#%^&%&$%
+#5.--> case sensitive
+#6.-->keywords are not allowed as variables
+
+
+
+#ways to define variables
+ #a=8
+ #b=9
+ #print(a,b)
+#a=7,b=9 #error
+#print(a,b)
+
+
+
+
+"""
+"""
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+"""
+"""
+1.} commenting line is used for making documentation in python file
+2.} when the line is commented,that line will not be considered as a part of the code
+3.}when the program is excecuted, commented line will not be in the part of the excecution
+-------->single line commenting can be done using# at the beginning of the line
+ eg: #print("hello world")
+----> multiple line commenting  can be done using code the lines b/w quoytes will be commented
+"""
+"""
+eg:
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+"""
+#print("hello world")
+#import this
+#print(this)
+# import is a keyword which is used to communicated between the python files.
+#import antigravity
+a,b=78
+print(a,b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
